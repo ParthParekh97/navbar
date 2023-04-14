8c2c3d0181cc96e473a56512013f45b97efae45a
Parth Parekh <parthkparekh2014@gmail.com>
2023-04-14 13:21:39 -0400

    minute change

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,7 +11,7 @@ function NavBar() {
       <nav className="navbar">
         <div className="nav-container">
           <NavLink exact to="/" className="nav-logo">
-            CodeBucks
+            Parth Parekh 
             <i className="fas fa-code"></i>
           </NavLink>
 
